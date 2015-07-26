8bb729b636cb9cbb5de8cc7c04031b9ba1536b96
Wade Ellis <wadeellis@dynip-245-122.massey.ac.nz>
2015-07-27 10:31:58 +1200

    Initial Commit

---
+++ Test.py
@@ -0,0 +1,7 @@
+__author__ = 'Wade Ellis'
+authorship_string = "{} created on {} by {} ({})\n{}\n".format(
+    "Test.py", "27/07/15", __author__, 15049749, "-----" * 15) \
+    if __name__ == '__main__' else ""
+print(authorship_string, end="")
+
+print("banter")
